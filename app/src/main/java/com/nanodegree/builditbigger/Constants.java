bf670b3880063e362a4a0af52db2b36aed87c6da
Yogesh <yogeshmadaan100@gmail.com>
2016-02-21 23:50:51 +0530

    Gradle test task added

--- app/src/main/java/com/nanodegree/builditbigger/Constants.java
+++ app/src/main/java/com/nanodegree/builditbigger/Constants.java
@@ -14,13 +14,13 @@ public class Constants {
      * Debug: link to local database
      * Production : link to the server
      */
-    public static final Environment environment = Environment.PRODUCTION;
+    public static final Environment environment = Environment.DEBUG;
 
     /***
      * Define the local server IP
      */
-    public static final String ENDPOINT_IP = "192.168.1.2";
-    public static final String ENDPOINT_ROOT_URL = "http://" + ENDPOINT_IP + ":8080/_ah/api";
+    public static final String ENDPOINT_IP = "192.168.1.1";
+    public static final String ENDPOINT_ROOT_URL = "http://" + ENDPOINT_IP + ":8888/_ah/api";
 
     public static final String PRODUCTION_URL = "rare-hull-122808.appspot.com";
     public static final String ENDPOINT_PRODUCTION_ROOT_URL = "https://" + PRODUCTION_URL + "/_ah/api";
